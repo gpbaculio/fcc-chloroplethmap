5592800a858c6bcd2e61e98c17d0fd4b7081d357
Glendon Philipp <gpbaculio@gmail.com>
2019-05-18 00:24:21 +0800

    initialize project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import * as topojson from 'topojson';
+import { educUrl, countyUrl } from './constants';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface topoRes {
+  features: any;
+}
+class App extends Component {
+  state = {
+    data: null
+  };
+  componentDidMount = async () => {
+    try {
+      const data = await axios.get(educUrl).then(async res => {
+        const { data } = await axios.get(countyUrl);
+        const datas = await topojson.feature(data, data.objects.counties);
+        console.log('educUrl', datas.features);
+        return res;
+      });
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+  render() {
+    return <div />;
+  }
 }
 
 export default App;
---
+++ src/constants.tsx
@@ -0,0 +1,4 @@
+export const educUrl =
+  'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json';
+export const countyUrl =
+  'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json';
